f6b84bea6ef1ef5d70744556755712d23c6c771e
Blake <contact@blake-cooper.com>
2019-08-01 15:08:28 -0400

    More strategies + cleanup

--- js/utilities.js
+++ js/utilities.js
@@ -1,22 +1,25 @@
 const NUMBER_OF_POSSIBLE_COLORS = 16581375;
 const HEXIDECIMAL = 16;
 
-function $(id)
-   	{
+/* Range: 0-255 */
+const MINIMAL_DIFFERENCE_IN_BRIGHTNESS = 25;
+
+function $(id) {
    	return document.getElementById(id);
-   	}
+}
 
-function getRandomStrategy()
-	{
+function getRandomStrategy() {
 	return strategies[random(strategies.length)];
-	}
+}
 
-function getRandomColor()
-	{
-	return random(NUMBER_OF_POSSIBLE_COLORS).toString(HEXIDECIMAL);
-	}
+function getRandomColor() {
+	return hexidecimal(random(NUMBER_OF_POSSIBLE_COLORS));
+}
 
-function random(value)
-	{
+function random(value) {
 	return Math.floor(Math.random() * value);
-	}
+}
+
+function hexidecimal(value) {
+	return value.toString(HEXIDECIMAL);
+}
